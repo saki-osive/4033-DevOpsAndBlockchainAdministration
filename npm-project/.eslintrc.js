e4f0195629afe6b53bd091f5a87fadc0d6263d19
arcturus <arcturus@pop-os.localdomain>
2024-02-19 19:19:58 -0500

    add secret key

---
+++ npm-project/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        // Add your ESLint rules here
+    },
+};
